603c36327fb39cb9069a33ca2655d9116c816161
BamboWu <qw2699@utexas.edu>
2022-05-07 00:18:36 +0000

    Regenerate __self address so can migrate uthread
    Compiler might do some optimization to reuse the tpidr_el0 value loaded
    in a general-purpose register, but from the first time thread_self() is
    called, to the second time invokation, the uthread might have moved to
    another kthread, and the __self of the previous kthread is pointing to
    something else.

--- inc/runtime/thread.h
+++ inc/runtime/thread.h
@@ -30,7 +30,29 @@ extern __thread thread_t *__self;
  */
 inline thread_t *thread_self(void)
 {
+#if defined(__aarch64__) && defined(TLS_LOCAL_EXEC)
+    thread_t *tmp;
+    unsigned int tmp0; /* transient register */
+    asm volatile(
+            "mrs  %0,  TPIDR_EL0                       \r\n"
+            "ldr  %1,  [%0, #:tprel_lo12: __self]      \r\n"
+            : "=r"(tmp0), "=r"(tmp) /* let compiler pick %0 */
+            : :);
+    return tmp;
+#elif defined(__aarch64__)
+    thread_t *tmp;
+    unsigned int tmp0; /* transient register */
+    asm volatile(
+            "mrs  %0,  TPIDR_EL0                          \r\n"
+            "adrp %1,  _GLOBAL_OFFSET_TABLE_              \r\n"
+            "ldr  %1,  [%1, #:gottprel_lo12: __self]      \r\n"
+            "ldr  %1,  [%1, %0]                           \r\n"
+            : "=r"(tmp0), "=r"(tmp) /* let compiler pick %0 */
+            : :);
+    return tmp;
+#else
     return __self;
+#endif
 }
 
 
